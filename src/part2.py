e5aeb3c7ef0ac765c0ca5193d84dad05829b8703
youcefAZ <youcefdota@gmail.com>
2021-12-02 18:47:51 +0100

    added load mask function

--- src/part2.py
+++ src/part2.py
@@ -2,9 +2,12 @@ import cv2
 import numpy as np
 from numpy.lib.function_base import append
 
+lightDpath="data/light_directions.txt"
+lightIpath="data/light_intensities.txt"
+maskPath="data/mask.png"
 
 def load_lightSources(n):
-    with open("data/light_directions.txt","r") as file:
+    with open(lightDpath,"r") as file:
         mat=np.empty((n,3),float)
         i=0
         for line in file:
@@ -19,7 +22,7 @@ def load_lightSources(n):
 
 
 def load_lightintensity(n):
-    with open("data/light_intensities.txt","r") as file:
+    with open(lightIpath,"r") as file:
         mat=np.empty((n,3),float)
         i=0
         for line in file:
@@ -33,5 +36,17 @@ def load_lightintensity(n):
     return mat
 
 
-light_source=load_lightSources(96)
-print(light_source)
+def load_objMask():
+    img = cv2.imread(maskPath,cv2.IMREAD_GRAYSCALE)
+    h,w=img.shape
+    mask=np.zeros((h,w),np.uint16)
+    for j in range(w):
+        for i in range(h):
+            if img[i,j]==255:
+                mask[i,j]=1
+    
+    return mask
+
+
+#light_source=load_lightSources(96)
+mask=load_objMask()
