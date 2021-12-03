d0da73b34b285f88abcd4d49fd6b18339f98a8e7
Abdelhak Aissat <abdelhak2478@gmail.com>
2021-12-03 12:11:00 +0100

    Delete n argument and minor code changes
    the n shouldn't be a parameter since it's the length of the file.

--- src/part2.py
+++ src/part2.py
@@ -1,33 +1,44 @@
 import cv2
 import numpy as np
-from numpy.lib.function_base import append
 
 lightDpath="data/light_directions.txt"
 lightIpath="data/light_intensities.txt"
 maskPath="data/mask.png"
 
-def load_lightSources(n):
+def load_lightSources():
+    """Check the light_direction file and transform into a matrix 
+        Object.
+    """
+    
     with open(lightDpath,"r") as file:
-        mat=np.empty((n,3),float)
+        n = len(file.readlines())
         i=0
+        #put the file pointer into the beggining of the file(readlines puts it in the end).
+        file.seek(0)
+        mat=np.empty((n,3),float)
         for line in file:
             elements=line.split()
-            print(elements[0])
             mat[i,0]=elements[0]
             mat[i,1]=elements[1]
             mat[i,2]=elements[2]
             i+=1
-            
+
     return mat
 
 
-def load_lightintensity(n):
+def load_lightintensity():
+    """Check the light_intensities file and transform into a matrix 
+        Object.
+    """
+
     with open(lightIpath,"r") as file:
-        mat=np.empty((n,3),float)
+        n = len(file.readlines())
         i=0
+        #put the file pointer into the beggining of thefile(readlines puts it in the end).
+        file.seek(0)
+        mat=np.empty((n,3),float)
         for line in file:
             elements=line.split()
-            print(elements[0])
             mat[i,0]=elements[0]
             mat[i,1]=elements[1]
             mat[i,2]=elements[2]
@@ -37,6 +48,7 @@ def load_lightintensity(n):
 
 
 def load_objMask():
+    """"""
     img = cv2.imread(maskPath,cv2.IMREAD_GRAYSCALE)
     h,w=img.shape
     mask=np.zeros((h,w),np.uint16)
@@ -48,5 +60,8 @@ def load_objMask():
     return mask
 
 
-#light_source=load_lightSources(96)
-mask=load_objMask()
+def main():
+    mask=load_objMask()
+
+if __name__ == "__main__":
+    main()
